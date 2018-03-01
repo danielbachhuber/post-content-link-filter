d963e47424690a9adbb9681bc528b9cf49d65e90
Daniel Bachhuber <daniel@handbuilt.co>
2018-03-01 06:33:41 -0800

    Initial commit

---
+++ post-content-link-filter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Post Content Link Filter
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     post-content-link-filter
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Post_Content_Link_Filter
+ */
+
+// Your code starts here.
